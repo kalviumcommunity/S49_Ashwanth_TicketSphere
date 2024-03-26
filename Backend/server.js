f8c6d65d09eed3f091033f429631fe175ac4bf35
Ashwanth2310 <ashwanth@rajakumar.net>
2024-03-26 14:36:07 +0530

    Added GET endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('Wassup')
+    res.send('TicketSphere')
 })
 app.get('/tickets',(req,res)=>{
     res.send('Viewing available tickets')
