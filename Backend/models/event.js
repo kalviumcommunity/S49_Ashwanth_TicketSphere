8240a980a3288b8410dbb5bb80ff63b5cb6e249f
Ashwanth2310 <ashwanth@rajakumar.net>
2024-05-10 11:47:31 +0530

    Sends user date to database when listing an event(Not fully wokring)

--- Backend/models/event.js
+++ Backend/models/event.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const eventSchema = new Schema({
-    eventName: {
-        type: String,
-        required: true
-    },
-    eventLocation: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: 0
-    },
-    poster: {
-        type: String, 
-        required: true
-    },
-    category: {
-        type: String,
-        required: true
-    }
+const eventSchema = new mongoose.Schema({
+  eventName: {
+    type: String,
+    required: true,
+  },
+  eventLocation: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  poster: {
+    type: String,
+    default: '',
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  sellerName: {
+    type: String,
+    required: true, 
+  },
 });
 
-const Event = mongoose.model('Event', eventSchema);
-
-module.exports = Event;
+module.exports = mongoose.model('Event', eventSchema);
