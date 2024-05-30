654a98e55cc9c41e43b9c096f04e98c60f2dcaa4
Ashwanth2310 <ashwanth@rajakumar.net>
2024-05-30 18:57:22 +0530

    Added a search bar

--- TicketSphere/src/components/Home.jsx
+++ TicketSphere/src/components/Home.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { Modal, Select } from 'antd';
-import "./home.css";
+import "./Home.css";
 import { SignedIn, useUser } from '@clerk/clerk-react';
 import Loader from './loader.jsx';
 
@@ -89,14 +89,39 @@ const Home = () => {
           <Select.Option value="Food Festival">Food Festival</Select.Option>
         </Select>
         <Link to="/events">
-          <button className='vav'>
-    <span>View all events</span>
-    <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="37" cy="37" r="35.5" stroke="black" stroke-width="3"></circle>
-        <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
+        <a href="#" class="button1">
+  <span class="button1__icon-wrapper">
+    <svg
+      viewBox="0 0 14 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="button1__icon-svg"
+      width="10"
+    >
+      <path
+        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+        fill="currentColor"
+      ></path>
     </svg>
-</button>
-        </Link>
+
+    <svg
+      viewBox="0 0 14 15"
+      fill="none"
+      width="10"
+      xmlns="http://www.w3.org/2000/svg"
+      class="button1__icon-svg button1__icon-svg--copy"
+    >
+      <path
+        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  </span>
+  Explore All Events
+</a>
+
+</Link>
+        
       </div>
       <b>Trending events 🔥</b>
       <br />
